3af82b91a9d500268af46a8a67d72881d17191e2
NikStack20 <codingnik20@gmail.com>
2026-07-31 00:59:49 +0530

    feat(config): setup main page, config, and assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
 import Navbar from "@/components/layout/Navbar";
-import Hero from "@/components/sections/Hero";
+import { Hero } from "@/components/sections/Hero";
+import { TrackRecord } from "@/components/sections/TrackRecord";
+import { EnterpriseEdge } from "@/components/sections/EnterpriseEdge";
+import { DomainExpertise } from "@/components/sections/DomainExpertise";
+import { CourseSegmentation } from "@/components/sections/CourseSegmentation";
+import { WhoShouldJoin } from "@/components/sections/WhoShouldJoin";
+import { CATFramework } from "@/components/sections/CATFramework";
+import { Testimonials } from "@/components/sections/Testimonials";
+import { DeliveryProcess } from "@/components/sections/DeliveryProcess";
+import { Companies } from "@/components/sections/Companies";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Hero />
+
+      <main>
+        <Hero />
+        <TrackRecord />
+        <EnterpriseEdge />
+        <DomainExpertise />
+        <CourseSegmentation />
+        <WhoShouldJoin />
+        <CATFramework />
+        <Testimonials />
+        <DeliveryProcess />
+        <Companies/>
+      </main>
     </>
   );
 }
